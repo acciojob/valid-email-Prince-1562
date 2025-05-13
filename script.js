169bc0d402335b2c426d52758c62721e80766fd5
Prince-1562 <princemakani1562@gmail.com>
2025-05-13 08:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validEmail(str) {
-  //your JS code here.
-}
+  if (!str) return false;
 
-// Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+  const emailRegex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
+
+  return emailRegex.test(str);
+}
